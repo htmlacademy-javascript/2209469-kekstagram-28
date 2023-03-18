586f81dc0abc7d9166ac109e290702fdc6276098
Keks <keks@htmlacademy.ru>
2023-03-18 18:32:09 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/functions.js
@@ -0,0 +1,39 @@
+const isMoreOrLess = (string, length) => {
+  return string.length <= length;
+}
+
+const isPalindrom = (string) => {
+  const tempString = string
+    .toLowerCase()
+    .replaceAll(' ', '');
+
+  let reverseString = '';
+  for (let i = tempString.length - 1; i >= 0; i--) {
+    reverseString += tempString.at(i);
+  }
+  return tempString === reverseString;
+};
+
+const findNumber = (string) => {
+  if (typeof string === 'number') {
+    return string;
+  }
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+};
+
+const myPadStart = (string, minLength, pad) => {
+  let result = string;
+  while (result.length < minLength) {
+    const newResultLength = result.length + pad.length;
+    const actualPad = newResultLength <= minLength ? pad : pad.slice(0, minLength - newResultLength);
+    result = actualPad + result;
+  }
+  return result;
+};
+
